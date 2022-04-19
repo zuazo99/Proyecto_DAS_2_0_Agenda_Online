6841ef7f7244bce8be9f84185df42e21894c9592
zuazo99 <gzuazo63@gmail.com>
2022-04-19 22:01:54 +0200

    Implementando la clase Register.java, el registro de usuario en la base de datos

--- app/src/main/java/com/gzuazo/myapplication/Register.java
+++ app/src/main/java/com/gzuazo/myapplication/Register.java
@@ -1,9 +1,11 @@
 package com.gzuazo.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Patterns;
@@ -13,7 +15,14 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class Register extends AppCompatActivity {
 
@@ -55,13 +64,14 @@ public class Register extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 //
+                validarDatos();
             }
         });
 
         TengounacuentaTXT.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                startActivity(new Intent(Register.this, Login.class));
             }
         });
     }
@@ -81,6 +91,8 @@ public class Register extends AppCompatActivity {
             Toast.makeText(this, getString(R.string.toastIngreseContraseña), Toast.LENGTH_SHORT).show();
         }else if (TextUtils.isEmpty(confirmarPassword)){
             Toast.makeText(this, getString(R.string.toastIngreseConfirmeContraseña), Toast.LENGTH_SHORT).show();
+        }else if (!password.equals(confirmarPassword)){
+            Toast.makeText(this, getString(R.string.compararPassword), Toast.LENGTH_SHORT).show();
         }
         else{
             crearCuenta();
@@ -88,7 +100,66 @@ public class Register extends AppCompatActivity {
     }
 
     private void crearCuenta() {
+        progressDialog.setMessage(getString(R.string.messageProgressDialog));
+        progressDialog.show();
 
+        // Crear un usuario en FireBase
+
+        firebaseAuth.createUserWithEmailAndPassword(correo, password)
+                    .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                        @Override
+                        public void onSuccess(AuthResult authResult) { // Permite gestionar el codigo necesario para realizar un registro exitoso
+                            // Registro exitoso
+                            guardarInformacion();
+
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // aqui entramos si ha ocurrido un error a la hora de crear el usuario
+                        progressDialog.dismiss();
+                        Toast.makeText(Register.this, getString(R.string.onFailure)+e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+        });
+    }
+
+    private void guardarInformacion() {
+        progressDialog.setMessage(getString(R.string.messageSaveProgressDialog));
+        progressDialog.dismiss();
+
+        //Obtener la identificacion del usuario actual
+        String uid = firebaseAuth.getUid();
+
+        HashMap<String, String> datos = new HashMap<>();
+        datos.put("uid", uid);
+        datos.put("correo", correo);
+        datos.put("nombre", nombre);
+        datos.put("password", password);
+
+        // Obtenemos la referencia de la base de datos
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Usuarios");
+        databaseReference.child(uid)
+                .setValue(datos)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Toast.makeText(Register.this, getString(R.string.messageCuentaCreada), Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(Register.this, MenuPrincipal.class));
+                        finish();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    progressDialog.dismiss();
+                    Toast.makeText(Register.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+                });
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return super.onSupportNavigateUp();
     }
 
     private void setViews(){
