7fe7afe2b197533fad8bec58936eb6282b68919d
zuazo99 <gzuazo63@gmail.com>
2022-04-19 19:26:04 +0200

    Creacion de la clase activity_register.xml para el registro de usuarios

--- app/src/main/java/com/gzuazo/myapplication/MainActivity.java
+++ app/src/main/java/com/gzuazo/myapplication/MainActivity.java
@@ -2,13 +2,35 @@ package com.gzuazo.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnLogin, btnRegistro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogin = findViewById(R.id.btn_login);
+        btnRegistro = findViewById(R.id.btn_Registro);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, Login.class));
+            }
+        });
+
+        btnRegistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, Register.class));
+            }
+        });
     }
 }
