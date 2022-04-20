ba7a9e459f7d4878798c7e99f3d5a14829e26c79
zuazo99 <gzuazo63@gmail.com>
2022-04-20 11:47:37 +0200

    Gestionando si el usuario ha iniciado sesion o no la ha iniciado

--- app/src/main/java/com/gzuazo/myapplication/LoadScreen.java
+++ app/src/main/java/com/gzuazo/myapplication/LoadScreen.java
@@ -6,7 +6,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class LoadScreen extends AppCompatActivity {
+
+
+    // Comprobar si el usuario tiene la sesion iniciada
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +24,20 @@ public class LoadScreen extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(LoadScreen.this, MainActivity.class));
-                finish();
+                /*startActivity(new Intent(LoadScreen.this, MainActivity.class));
+                finish();*/
+                verificarUsuario();
             }
         }, Tiempo);
+    }
+
+    private void verificarUsuario(){
+        FirebaseUser user = firebaseAuth.getCurrentUser(); // Estamos obteniendo el usuario actual(previamente se registro y inicio sesion)
+        if (user == null){
+            startActivity(new Intent(LoadScreen.this, MainActivity.class));
+            finish();
+        }else{
+            startActivity(new Intent(LoadScreen.this, MenuPrincipal.class));
+        }
     }
 }
