f69106a3f13187a87ee65f2ddd83acbfdfe95f36
zuazo99 <gzuazo63@gmail.com>
2022-04-21 09:25:39 +0200

    MenuPrincipal.java obtener los datos del usuario que ha iniciado sesion

--- app/src/main/java/com/gzuazo/myapplication/MenuPrincipal.java
+++ app/src/main/java/com/gzuazo/myapplication/MenuPrincipal.java
@@ -1,28 +1,46 @@
 package com.gzuazo.myapplication;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MenuPrincipal extends AppCompatActivity {
 
+    TextView nombresPrincipal, correoPrincipal;
     Button btnCerrarSesion;
     FirebaseAuth firebaseAuth;
     FirebaseUser user;
+    ProgressBar progressBarDatos;
+
+    DatabaseReference usuarios; // Para leer o escribir en la base de datos
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle(getString(R.string.tituloMenuPrincipalActionBar));
 
-        btnCerrarSesion = findViewById(R.id.CerrarSesion);
+        inicializarViews();
+
+        //Inicializar Firebase
+        usuarios = FirebaseDatabase.getInstance().getReference("Usuarios");
         firebaseAuth = FirebaseAuth.getInstance();
         user = firebaseAuth.getCurrentUser();
 
@@ -34,9 +52,64 @@ public class MenuPrincipal extends AppCompatActivity {
         });
     }
 
+    @Override
+    protected void onStart() {
+        validateInicioSesion();
+        super.onStart();
+    }
+
+    private void validateInicioSesion(){
+        if (user != null){
+            // el usuario ha iniciado sesion
+            cargarDatos();
+        }else{
+
+            startActivity(new Intent(MenuPrincipal.this, MainActivity.class));
+            finish();
+        }
+    }
+
+    private void cargarDatos(){
+        usuarios.child(user.getUid()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) { // Nos permite leer a tiempo real los datos de ese usuario
+                // si el usuario existe
+                if(snapshot.exists()){
+                    // progressBar se oculta
+                    progressBarDatos.setVisibility(View.GONE);
+                    // textviews se ven
+                    nombresPrincipal.setVisibility(View.VISIBLE);
+                    correoPrincipal.setVisibility(View.VISIBLE);
+
+                    // Obtener datos
+
+                    String nombres = ""+ snapshot.child("nombre").getValue();
+                    String correo = ""+ snapshot.child("correo").getValue();
+
+                    // settear datos en los textViews
+                    nombresPrincipal.setText(nombres);
+                    correoPrincipal.setText(correo);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+
     private void salirApp() {
         firebaseAuth.signOut();
         startActivity(new Intent(MenuPrincipal.this, MainActivity.class));
         Toast.makeText(this, getString(R.string.salirApp), Toast.LENGTH_SHORT).show();
+    }
+
+    private void inicializarViews(){
+        nombresPrincipal = findViewById(R.id.NombresPrincipal);
+        correoPrincipal = findViewById(R.id.CorreoPrincipal);
+        btnCerrarSesion = findViewById(R.id.CerrarSesion);
+        progressBarDatos = findViewById(R.id.progressBarDatos);
     }
 }
