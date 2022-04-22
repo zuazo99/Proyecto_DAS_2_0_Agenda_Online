fc5ac3c1f21c2c8f52795b1d5527051f8bc8d434
zuazo99 <gzuazo63@gmail.com>
2022-04-22 12:05:34 +0200

    implementado el calendario con DatePickerDialog

--- app/src/main/java/com/gzuazo/myapplication/AgregarNota/AgregarNota.java
+++ app/src/main/java/com/gzuazo/myapplication/AgregarNota/AgregarNota.java
@@ -4,11 +4,14 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -17,6 +20,7 @@ import com.gzuazo.myapplication.R;
 
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.util.Calendar;
 import java.util.Locale;
 
 public class AgregarNota extends AppCompatActivity {
@@ -24,6 +28,8 @@ public class AgregarNota extends AppCompatActivity {
     TextView id_Usuario, correo_Usuario, fechaHoraActual, fecha, estado;
     EditText tituloNota, descripcion;
     Button btn_Calendario;
+
+    int dia, mes, anio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +40,47 @@ public class AgregarNota extends AppCompatActivity {
         inicializarViews();
         getDatosUsuario();
         getFechaHoraActual();
+
+        btn_Calendario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Calendar calendario = Calendar.getInstance();
+
+                dia = calendario.get(Calendar.DAY_OF_MONTH);
+                mes = calendario.get(Calendar.MONTH);
+                anio = calendario.get(Calendar.YEAR);
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(AgregarNota.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int AnioSeleccion, int MesSeleccion, int DiaSeleccion) {
+                        // DatePickerDialog no devuelve el dia y el mes en un formato adecuado, tenemos que adecuarlo nosotros
+                        // Para ello, en primer lugar declaramos dos Strings vacios:
+                        String diaFormatted, mesFormatted;
+
+                        // 2. Obtener dia
+                        if (DiaSeleccion < 10){
+                            diaFormatted = "0" + String.valueOf(DiaSeleccion);
+                            // Antes: 9/12/2022 - Ahora 09/12/2022
+                        }else{
+                            diaFormatted = String.valueOf(DiaSeleccion);
+                        }
+
+                        //Obtener el mes
+                        int Mes = MesSeleccion + 1;
+
+                        if (MesSeleccion < 10){
+                            mesFormatted = "0" + String.valueOf(Mes);
+                        }else{
+                            mesFormatted = String.valueOf(Mes);
+                        }
+                        // Seteamos la fecha en el TExtView
+                        fecha.setText(diaFormatted + "/" + mesFormatted + "/" + AnioSeleccion);
+                    }
+                }
+                ,anio, mes, dia);
+                datePickerDialog.show();
+            }
+        });
     }
     private void inicializarActionBar(){
         //ActionBar para que el usuario pueda volver atras, mediante una flecha
